b4639f517c380a79c6b676931bf0eae5e0399498
daniltsy <cli0300@mail.ru>
2023-02-19 11:15:27 +0300

    added parametrize to check names and costs match in different pages of products

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,19 @@
 from pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('params', ["?promo=offer0",
+                                  "?promo=offer1",
+                                  "?promo=offer2",
+                                  "?promo=offer3",
+                                  "?promo=offer4",
+                                  "?promo=offer5",
+                                  "?promo=offer6",
+                                  pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+                                  "?promo=offer8",
+                                  "?promo=offer9"])
+def test_guest_can_add_product_to_cart(browser, params):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{params}"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_product_added_to_cart()
